4f60bac3f0defd5b3f7cbbdab30938e879cec8fa
Melvin García <neodarknsf@gmail.com>
2019-04-13 21:51:14 -0600

    Essential -> Vortex

--- config/ota.mk
+++ config/ota.mk
@@ -4,14 +4,14 @@ ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 CUSTOM_OTA_VERSION_CODE := pie
 
 CUSTOM_PROPERTIES += \
-    org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
+    org.vortex.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
     sys.ota.disable_uncrypt=1
 
 PRODUCT_PACKAGES += \
     Updates
 
 PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/org.pixelexperience.ota.xml:system/etc/permissions/org.pixelexperience.ota.xml
+    vendor/aosp/config/permissions/org.vortex.ota.xml:system/etc/permissions/org.vortex.ota.xml
 
 endif
 endif
